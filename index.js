0d625c5956f423fdb770c10388b5d20ce09e20ef
endless <35359567+Endless5F@users.noreply.github.com>
2023-07-31 23:39:11 +0800

    init react native config

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {AppRegistry, StyleSheet, Text, View} from 'react-native';
+import {HelloWorld} from "./MixReactNative/HelloWorld";
+
+
+AppRegistry.registerComponent(
+  'MyReactNativeApp',
+  () => HelloWorld,
+);
