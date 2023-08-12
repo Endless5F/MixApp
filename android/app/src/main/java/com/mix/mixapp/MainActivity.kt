c3e0463dd0ec4a1e043a036ba3ee8d85afdf3d28
endless <35359567+Endless5F@users.noreply.github.com>
2023-08-12 22:50:27 +0800

    update react native config

--- android/app/src/main/java/com/mix/mixapp/MainActivity.kt
+++ android/app/src/main/java/com/mix/mixapp/MainActivity.kt
@@ -1,43 +1,127 @@
 package com.mix.mixapp
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.mix.mixapp.ui.theme.MixAndroidAppTheme
+import android.provider.Settings
+import android.view.KeyEvent
+import androidx.appcompat.app.AppCompatActivity
+import com.facebook.hermes.reactexecutor.HermesExecutorFactory
+import com.facebook.react.PackageList
+import com.facebook.react.ReactInstanceManager
+import com.facebook.react.ReactPackage
+import com.facebook.react.ReactRootView
+import com.facebook.react.common.LifecycleState
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler
+import com.facebook.soloader.SoLoader
 
-class MainActivity : ComponentActivity() {
+/**
+ *
+ * 资料：https://blog.csdn.net/qq_36828822/article/details/127398891
+ */
+class MainActivity : AppCompatActivity(), DefaultHardwareBackBtnHandler {
+
+    private val OVERLAY_PERMISSION_REQ_CODE = 1 // 任写一个值
+    private var mReactRootView: ReactRootView? = null
+    private var mReactInstanceManager: ReactInstanceManager? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MixAndroidAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+        requestAlertWindowPermission()
+
+        runCatching {
+            SoLoader.init(this, false)
+        }
+
+        mReactRootView = ReactRootView(this)
+        val packages: List<ReactPackage> = PackageList(application).packages
+        // 有一些第三方可能不能自动链接，对于这些包我们可以用下面的方式手动添加进来：
+        // packages.add(new MyReactNativePackage());
+        // 同时需要手动把他们添加到`settings.gradle`和 `app/build.gradle`配置文件中。
+
+        mReactInstanceManager = ReactInstanceManager.builder()
+            .setApplication(application)
+            .setCurrentActivity(this)
+            .setBundleAssetName("main.bundle")
+            .setJSMainModulePath("index")
+            .addPackages(packages)
+            // fix java.lang.NoClassDefFoundError: com.facebook.react.jscexecutor.JSCExecutor
+            // at com.facebook.react.ReactInstanceManagerBuilder.getDefaultJSExecutorFactory
+            .setJavaScriptExecutorFactory(HermesExecutorFactory())
+            .setUseDeveloperSupport(BuildConfig.DEBUG)
+            .setInitialLifecycleState(LifecycleState.RESUMED)
+            .build()
+
+        // 注意这里的MyReactNativeApp 必须对应"index.js"中的
+        // "AppRegistry.registerComponent()"的第一个参数
+        mReactRootView?.startReactApplication(mReactInstanceManager, "MixReactNativeApp", null)
+
+        setContentView(mReactRootView)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mReactInstanceManager?.onHostPause(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mReactInstanceManager?.onHostResume(this, this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mReactInstanceManager?.onHostDestroy(this)
+        mReactRootView?.unmountReactApplication()
+    }
+
+    override fun onBackPressed() {
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager?.onBackPressed()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onKeyUp(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_MENU && mReactInstanceManager != null) {
+            mReactInstanceManager?.showDevOptionsDialog()
+            return true
+        }
+        return super.onKeyUp(keyCode, event)
+    }
+
+    override fun invokeDefaultOnBackPressed() {
+        super.onBackPressed()
+    }
+
+    private fun requestAlertWindowPermission() {
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                // 悬浮窗权限判断
+                if (Settings.canDrawOverlays(this)) {
+                    return
+                }
+                val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
+                intent.data = Uri.parse("package:$packageName")
+                startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == OVERLAY_PERMISSION_REQ_CODE) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (!Settings.canDrawOverlays(this)) {
+                    // SYSTEM_ALERT_WINDOW permission not granted
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MixAndroidAppTheme {
-        Greeting("Android")
+        mReactInstanceManager?.onActivityResult(this, requestCode, resultCode, data)
     }
 }
