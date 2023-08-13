f4518282d49bc4d41128aed593cf1eb25c2f76df
endless <35359567+Endless5F@users.noreply.github.com>
2023-08-13 09:59:37 +0800

    调整react native目录层级

--- MixAndroidApp/app/src/main/java/com/mix/mixapp/MixReactNativeActivity.kt
+++ MixAndroidApp/app/src/main/java/com/mix/mixapp/MixReactNativeActivity.kt
@@ -8,13 +8,13 @@ import android.provider.Settings
 import android.view.KeyEvent
 import androidx.appcompat.app.AppCompatActivity
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory
-import com.facebook.react.PackageList
 import com.facebook.react.ReactInstanceManager
 import com.facebook.react.ReactPackage
 import com.facebook.react.ReactRootView
 import com.facebook.react.common.LifecycleState
 import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler
 import com.facebook.soloader.SoLoader
+import com.mix.mixapp.react.PackageList
 
 /**
  *
--- index-src.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {AppRegistry, StyleSheet, Text, View} from 'react-native';
-import {HelloWorld} from "./MixReactNative/HelloWorld";
-
-
-AppRegistry.registerComponent(
-  'MixReactNativeApp',
-  () => HelloWorld,
-);
