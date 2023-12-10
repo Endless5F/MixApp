2b2f9986d69e6a04ea2b6a72578defc5768942e6
endless <35359567+Endless5F@users.noreply.github.com>
2023-12-10 22:18:34 +0800

    Compose SearchTitleBar

--- MixAndroidApp/app/src/main/java/com/mix/mixapp/MainActivity.kt
+++ MixAndroidApp/app/src/main/java/com/mix/mixapp/MainActivity.kt
@@ -4,16 +4,43 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.PreviewActivity
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat.startActivity
 import com.mix.mixapp.ui.theme.MixAndroidAppTheme
 import io.flutter.embedding.android.FlutterActivity
@@ -56,6 +83,64 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     MixAndroidAppTheme {
-        Greeting("Android")
+        SearchTitleBar()
+    }
+}
+
+@Composable
+fun SearchTitleBar() {
+    var text by remember {
+        mutableStateOf("aaa")
+    }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFD3D3D3))
+    ) {
+        BasicTextField(
+            value = text,
+            onValueChange = {
+                text = it
+            },
+            modifier = Modifier
+                .padding(horizontal = 10.dp)
+                .background(Color.White, CircleShape)
+                .height(30.dp)
+                .fillMaxWidth(),
+            decorationBox = { innerTextField ->
+                Row(
+                    Modifier
+                        .padding(vertical = 2.dp, horizontal = 8.dp)
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(imageVector = Icons.Filled.Search, contentDescription = "")
+                    Box(
+                        Modifier
+                            .padding(horizontal = 10.dp)
+                            .weight(1f),
+                        contentAlignment = Alignment.CenterStart
+                    ) {
+                        if (text.isEmpty()) {
+                            Text(
+                                text = "请输入点东西看看吧",
+                                style = TextStyle(
+                                    color = Color.Black
+                                )
+                            )
+                        }
+                        innerTextField()
+                    }
+                    if (text.isNotEmpty()) {
+                        IconButton(
+                            onClick = { text = "" },
+                            modifier = Modifier.size(16.dp)
+                        ) {
+                            Icon(imageVector = Icons.Filled.Close, contentDescription = "")
+                        }
+                    }
+                }
+            }
+        )
     }
 }
